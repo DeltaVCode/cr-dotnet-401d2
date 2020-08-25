d0690d436df680efac2f09ecddcd8cba03d9621c
Keith Dahlby <keith@newbo.co>
2020-08-25 10:06:37 -0500

    Show how we can manipulate Program args

---
+++ Class02/Demo/FizzBuzz/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArgsDemo(args);
+        }
+
+        static void ArgsDemo(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("{0}: {1}", i, args[i]);
+            }
+        }
+    }
+}
