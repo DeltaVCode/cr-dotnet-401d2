88f503fa1584e8334cea29d5ae28507fe82ec029
Keith Dahlby <keith@newbo.co>
2020-08-26 11:42:54 -0500

    Show read/write file

--- Class03/Demo/DemoApp/Program.cs
+++ Class03/Demo/DemoApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace DemoApp
 {
@@ -6,7 +7,30 @@ namespace DemoApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Ternary conditional operator
+            string path = args.Length > 0 ? args[0] : "hello.txt";
+
+            const string message = "Hello World!";
+
+            // File.WriteAllText(path, message);
+            File.WriteAllLines(path, new[] { message });
+            // File.WriteAllBytes(path, new byte[] { 0x41, 0x20, 0x66, 0x0A, 0x0D });
+
+            Console.WriteLine($"Wrote '{message}' to '{path}'");
+
+            Console.WriteLine($"Reading from '{path}' as lines");
+
+            string[] lines = File.ReadAllLines(path);
+            Array.ForEach(lines, line => Console.WriteLine(line));
+
+            Console.WriteLine($"Reading from '{path}' as text");
+
+            string text = File.ReadAllText(path);
+            Console.WriteLine(text);
+            Console.WriteLine($"Text length: {text.Length}");
+
+            byte[] bytes = File.ReadAllBytes(path);
+            Array.ForEach(bytes, b => Console.WriteLine(b.ToString("X2")));
         }
     }
 }
