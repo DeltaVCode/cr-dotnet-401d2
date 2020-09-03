5a14fec31220a1ded31e5b0bf86037b6e747c65b
Keith Dahlby <keith@newbo.co>
2020-09-03 12:24:00 -0500

    Add Lab09 JSON read example

---
+++ Class09/Demo/Demo.Tests/Lab09GeoJsonTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Demo.Tests
+{
+    public class Lab09GeoJsonTests
+    {
+        [Fact]
+        public void Can_read_file()
+        {
+            // Arrange
+            string fileName = "data.json";
+            string data = File.ReadAllText(fileName);
+            Assert.NotEmpty(data);
+
+            // Act
+            // Install-Package Newtonsoft.Json
+            RootObject root = JsonConvert.DeserializeObject<RootObject>(data);
+
+            // Assert
+            Assert.Equal("FeatureCollection", root.type);
+
+            Assert.NotEmpty(root.features);
+        }
+    }
+
+    internal class RootObject
+    {
+        public string type { get; set; }
+        public List<Feature> features { get; set; }
+    }
+
+    public class Feature
+    {
+        public string type { get; set; }
+        // TODO: geometry, properties
+    }
+}
