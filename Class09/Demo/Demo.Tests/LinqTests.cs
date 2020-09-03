5d47de5e28c9ee26eba591117daf3d60f5c9e83e
Keith Dahlby <keith@newbo.co>
2020-09-03 12:03:24 -0500

    Group by example

--- Class09/Demo/Demo.Tests/LinqTests.cs
+++ Class09/Demo/Demo.Tests/LinqTests.cs
@@ -77,9 +77,54 @@ namespace Demo.Tests
             numbers.Insert(8, 25);
 
             // numbers.reduce((acc, next) => acc + next, 0)
-            int sum = numbers.Aggregate(0, (acc, next) => acc + next);
+            int sum = numbers.Distinct().Aggregate(0, (acc, next) => acc + next);
 
-            Assert.Equal(1297, sum);
+            Assert.Equal(1287, sum);
+        }
+
+        [Fact]
+        public void Can_group_by_Author()
+        {
+            Book[] books = new[]
+            {
+                new Book {Title = "HP 1", Author = "JKR" },
+                new Book {Title = "HP 2", Author = "JKR" },
+                new Book {Title = "HP 3", Author = "JKR" },
+                new Book {Title = "HP 4", Author = "JKR" },
+                new Book {Title = "HP 5", Author = "JKR" },
+                new Book {Title = "HP 6", Author = "JKR" },
+                new Book {Title = "HP 7", Author = "JKR" },
+                new Book {Title = "LoTR1", Author = "JRRT" },
+                new Book {Title = "LoTR2", Author = "JRRT" },
+                new Book {Title = "LoTR3", Author = "JRRT" },
+                new Book {Title = "The Hobbit", Author = "JRRT" },
+                new Book {Title = "C# in Depth", Author = "Jon Skeet" },
+            };
+
+            var booksByAuthor =
+                from book in books
+                group book by book.Author into authorBooks
+                // Grouping { Key, Values }
+                // Grouping { JKR, Books = 1,2,3,5 }
+                // Grouping { JRRT, Books = 1,2,3 }
+                select new
+                {
+                    Author = authorBooks.Key, // Thing we grouped by
+                    BookCount = authorBooks.Count(),
+                    BookTitles = authorBooks.Select(book => book.Title).ToArray(),
+                };
+
+            // books.GroupBy(book => book.Author).Select(...)
+
+            var booksByAuthorList = booksByAuthor.ToList();
+            Assert.Equal(3, booksByAuthorList.Count);
+
+            Assert.Equal("JKR", booksByAuthorList[0].Author);
+            Assert.Equal(7, booksByAuthorList[0].BookCount);
+
+            Assert.Equal("JRRT", booksByAuthorList[1].Author);
+            Assert.Equal(4, booksByAuthorList[1].BookCount);
+            Assert.Equal(new[] { "LoTR1", "LoTR2", "LoTR3", "The Hobbit" }, booksByAuthorList[1].BookTitles);
         }
     }
 
