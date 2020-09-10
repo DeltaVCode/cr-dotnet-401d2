8561e520e49d3f5b927f61a3124c225b37bcc14e
Keith Dahlby <keith@newbo.co>
2020-09-10 11:00:17 -0500

    Add IStudentRepsitory and Controller

---
+++ Class13/Demo/Web/Services/IStudentRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Web.Models;
+
+namespace Web.Services
+{
+    public interface IStudentRepository
+    {
+        IEnumerable<Student> GetAll();
+
+        Student GetOneById(int id);
+
+        void Create(Student student);
+
+        void UpdateOneById(int id, Student student);
+
+        void DeleteOneById(int id);
+    }
+}
