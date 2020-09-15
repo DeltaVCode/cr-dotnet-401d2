64222020f801433429cb3bdecce93675f0be94c7
Keith Dahlby <keith@newbo.co>
2020-09-15 09:54:21 -0500

    Add Swashbuckle for Swagger

--- Class17/Demo/Web/Startup.cs
+++ Class17/Demo/Web/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Web.Data;
 using Web.Services;
 
@@ -46,6 +47,11 @@ namespace Web
             services.AddTransient<IStudentRepository, DatabaseStudentRepository>();
 
             services.AddTransient<ITechnologyRepository, DatabaseTechnologyRepository>();
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "DV Enrollment", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,6 +61,17 @@ namespace Web
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger(options =>
+            {
+                options.RouteTemplate = "/api/{documentName}/swagger.json";
+            });
+
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/api/v1/swagger.json", "DV Enrollments!");
+                options.RoutePrefix = "";
+            });
 
             app.UseRouting();
 
