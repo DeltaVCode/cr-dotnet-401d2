c64815e63e0f3922ea95aceefdba75488d51906d
Keith Dahlby <keith@newbo.co>
2020-09-28 11:04:59 -0500

    Basic MVC app

---
+++ Class26/Demo/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET: /
+        // GET: /Home
+        // GET: /Home/Index
+        public IActionResult Index()
+        {
+            _logger.LogInformation("Home page!");
+            return View();
+        }
+
+        // GET: /Home/PrivacyPrivacy
+        public IActionResult Privacy(int id)
+        {
+            return View();
+        }
+
+        // GET: /Home/Error
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
