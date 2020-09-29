05830e1d6094447f0b1f3fcaf72522a5098e0e2b
Keith Dahlby <keith@newbo.co>
2020-09-29 12:13:36 -0500

    Quick Login page

---
+++ Class27/Demo/WebApplication1/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication1.Models.Identity;
+
+namespace WebApplication1.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public LoginModel(SignInManager<ApplicationUser> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
+
+        public async Task OnGetAsync()
+        {
+            // Clear our cookie so we can actually log in
+            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(Input.Email, Input.Password, false, false);
+                if (result.Succeeded)
+                {
+                    return LocalRedirect("~/");
+                }
+
+                // Auth did not succeed; don't tell if it's username or password
+                ModelState.AddModelError(nameof(Input) + "." + nameof(Input.Email), "Invalid login.");
+            }
+
+            return Page();
+        }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            [Display(Name = "Remember me?")]
+            public bool RememberMe { get; set; }
+        }
+    }
+}
