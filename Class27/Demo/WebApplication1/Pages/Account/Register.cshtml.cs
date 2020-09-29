873a5f92769d87a3bbbedb76267a0b4c5ce74b5b
Keith Dahlby <keith@newbo.co>
2020-09-29 11:47:20 -0500

    Add Register form

---
+++ Class27/Demo/WebApplication1/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages.Account
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                return LocalRedirect("~/");
+            }
+
+            return Page();
+        }
+
+        [BindProperty]
+        public RegisterInput Input { get; set; }
+
+        public class RegisterInput
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [StringLength(100, MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm password")]
+            [Compare(nameof(Password))]
+            public string PasswordConfirm { get; set; }
+        }
+    }
+}
