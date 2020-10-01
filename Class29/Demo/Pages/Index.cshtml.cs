c11329bcd29d8e5e7a217f735942b956b375ae0f
Keith Dahlby <keith@newbo.co>
2020-10-01 13:41:54 -0500

    Demo upload to Azure blob storage

--- Class29/Demo/Pages/Index.cshtml.cs
+++ Class29/Demo/Pages/Index.cshtml.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Demo.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -8,16 +11,22 @@ namespace Demo.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IUploadService upload;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IUploadService upload)
         {
             _logger = logger;
+            this.upload = upload;
         }
 
         [TempData]
         public string UploadedFileName { get; set; }
 
+        [TempData]
+        public string UploadedFileLink { get; set; }
+
         [BindProperty]
+        [Required]
         public string Name { get; set; }
 
         [BindProperty]
@@ -27,9 +36,23 @@ namespace Demo.Pages
         {
         }
 
-        public IActionResult OnPost()
+        public async Task<IActionResult> OnPostAsync()
         {
-            UploadedFileName = Image?.FileName;
+            if (Image == null)
+            {
+                ModelState.AddModelError(nameof(Image), "Image is required!");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            // We can assume we got an Image
+            UploadedFileName = Image.FileName;
+
+            using var stream = Image.OpenReadStream();
+            UploadedFileLink = await upload.UploadFile(Image.FileName, stream);
 
             return LocalRedirect("/");
         }
---
+++ Class29/Demo/Services/AzureUploadService.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace Demo.Services
+{
+    public interface IUploadService
+    {
+        Task<string> UploadFile(string filename, Stream data);
+    }
+
+    public class AzureUploadService : IUploadService
+    {
+        public AzureUploadService(IConfiguration configuration)
+        {
+            var accountName = configuration["AzureStorageAccountName"];
+            var blobKey = configuration["AzureBlobKey"];
+
+            var storageCredentials = new StorageCredentials(accountName, blobKey);
+            var storageAccount = new CloudStorageAccount(storageCredentials, true);
+
+            CloudBlobClient = storageAccount.CreateCloudBlobClient();
+        }
+
+        public CloudBlobClient CloudBlobClient { get; }
+
+        public async Task<string> UploadFile(string filename, Stream data)
+        {
+            // Access the Storage Container
+            var container = CloudBlobClient.GetContainerReference("images");
+            await container.CreateIfNotExistsAsync();
+            await container.SetPermissionsAsync(new BlobContainerPermissions
+            {
+                // Allow anonymous public access to files *if you have the link*
+                PublicAccess = BlobContainerPublicAccessType.Blob,
+            });
+
+            // Actually upload
+            var blobFile = container.GetBlockBlobReference(filename);
+            await blobFile.UploadFromStreamAsync(data);
+            return blobFile.Uri.ToString();
+        }
+    }
+}
