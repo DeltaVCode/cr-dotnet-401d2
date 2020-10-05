f774c775461ee969b8de46642732b6333034d6a0
Keith Dahlby <keith@newbo.co>
2020-10-05 11:45:33 -0500

    Add TodoCount component

---
+++ Class31/Demo/TodoApi/Components/TodoCount.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Data;
+
+namespace TodoApi.Components
+{
+    public class TodoCount : ViewComponent
+    {
+        private readonly TodoDbContext db;
+
+        public TodoCount(TodoDbContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var incompleteCount = await db.Todo.CountAsync(t => !t.Completed);
+
+            return View(new TodoCountViewModel
+            {
+                IncompleteCount = incompleteCount,
+            });
+        }
+    }
+
+    public class TodoCountViewModel
+    {
+        public int IncompleteCount { get; set; }
+    }
+}
