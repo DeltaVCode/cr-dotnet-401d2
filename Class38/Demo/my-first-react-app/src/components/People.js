c36e8ce140fb4c6e7a24f048ccf6ec841486900d
Keith Dahlby <keith@newbo.co>
2020-10-14 10:19:02 -0500

    Demo toggle

--- Class38/Demo/my-first-react-app/src/components/People.js
+++ Class38/Demo/my-first-react-app/src/components/People.js
@@ -19,11 +19,47 @@ export default function People(props) {
     setPeople(people.filter((person, idx) => idx !== indexToRemove));
   }
 
+  function togglePersonAttending(index) {
+    console.log(index);
+    const newPeople = people.map((person, idx) => (
+      idx !== index ? person :
+        {
+          ...person,
+          attending: !person.attending
+        }
+    ));
+
+    setPeople(newPeople);
+    return;
+
+    // longer way, not so idiomatic
+    let arr = []
+    for(let i = 0; i < people.length; i++) {
+      if (i === index) {
+        arr[index] = {
+          // First, copy everything from the current person
+          ...people[i],
+
+          // Replace/set some properties
+          copiedAt: new Date(),
+          status: !people[i].status,
+        };
+      }
+      else {
+        arr[i] = people[i]
+      }
+    }
+    console.log(arr);
+    setPeople(arr);
+  }
+
   return (
     <>
       <h1>People!</h1>
       <PeopleForm onSave={savePerson} />
-      <PeopleList people={people} onDelete={deletePersonByIndex} />
+      <PeopleList people={people}
+         onDelete={deletePersonByIndex}
+         onRsvp={togglePersonAttending} />
     </>
   )
 }
@@ -57,13 +93,15 @@ function PeopleForm(props) {
 }
 
 function PeopleList(props) {
-  const { people, onDelete } = props;
+  const { people, onDelete, onRsvp } = props;
 
   return (
     <ul>
       {people.map((person, idx) => (
         <li key={idx}>
           {person.name}
+          <p>Attending: {person.attending ? 'Yes' : 'No'}</p>
+          <input type="checkbox" checked={person.attending} onChange={() => onRsvp(idx)} />
           <button onClick={() => onDelete(idx)}>Delete</button>
         </li>
       ))}
